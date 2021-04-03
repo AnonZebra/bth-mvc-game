d6819b2fe2fa04b7aac37f65c219bef0bd4fe54b
datalowe <datalowe@posteo.de>
2021-04-03 16:41:27 +0200

    replace error reporting call to be less version-specific/future-proof as per the PHP manual https://www.php.net/manual/de/function.error-reporting

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,7 +7,8 @@
 declare(strict_types=1);
 
 // Setup error reporting
-error_reporting(-1);                // Report all type of errors
+// error_reporting(-1);                // Report all type of errors
+error_reporting(E_ALL);                // https://www.php.net/manual/de/function.error-reporting
 ini_set("display_errors", "1");     // Display all errors
 
 // Start the session

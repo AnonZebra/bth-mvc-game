e9dd88c45dcd3a7862293ec46bc600b2206c84cf
datalowe <datalowe@posteo.de>
2021-05-09 00:52:25 +0200

    update migration to conform to mysql default id column generation definition

--- database/migrations/2021_05_07_001118_create_scores_table.php
+++ database/migrations/2021_05_07_001118_create_scores_table.php
@@ -16,8 +16,8 @@ class CreateScoresTable extends Migration
         Schema::create('scores', function (Blueprint $table) {
             $table->id();
             $table->integer('score');
-            $table->integer('player_id');
-            $table->integer('session_id');
+            $table->bigInteger('player_id')->unsigned();
+            $table->bigInteger('session_id')->unsigned();
             $table->foreign('player_id')->references('id')->on('players');
             $table->foreign('session_id')->references('id')->on('game_sessions');
         });

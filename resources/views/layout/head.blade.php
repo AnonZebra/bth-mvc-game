bf84ac0bfe87bd6224640aefaa717eb963a76d17
datalowe <datalowe@posteo.de>
2021-05-08 17:46:07 +0200

    reference stylesheet using url function

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -3,5 +3,5 @@
 <title>@yield('title')</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="#">
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}">
 @endsection('head')

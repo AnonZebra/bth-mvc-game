0d118ebf1ad555a0677cb34e16c4fc6e5965489b
datalowe <datalowe@posteo.de>
2021-05-09 00:56:40 +0200

    add sql script for adding books

---
+++ database/sql_scripts/dml.sql
@@ -0,0 +1,20 @@
+INSERT INTO books (title, isbn, author, cover_img_filename)
+VALUES
+(
+	'Harry Potter and the Philosopher''s Stone',
+	'0747532699',
+	'J. K. Rowling',
+	'harry.jpg'
+),
+(
+	'Night Watch',
+	'0385602642',
+	'Terry Pratchett',
+	'night-watch.jpg'
+),
+(
+	'The Hitchhiker''s Guide to the Galaxy',
+	'0330258648',
+	'Douglas Adams',
+	'hitchhiker.jpg'
+);
